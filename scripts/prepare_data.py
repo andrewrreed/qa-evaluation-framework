ec0636b57d1477a231fd092bcd25057f49e7c586
areed1242 <areed1242@gmail.com>
2020-06-09 19:22:07 -0400

    Update prepare_data.py

--- scripts/prepare_data.py
+++ scripts/prepare_data.py
@@ -1,4 +1,5 @@
 import logging
+import argparse
 import sys
 import os
 
@@ -12,11 +13,22 @@ logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s-%(levelname)s-%(message)s')
 
 
+# parse cli args
+parser = argparse.ArgumentParser()
+parser.add_argument('-RO', '--retriever-eval-only', help='1 if running retriever pipeline only, else 0')
+args = parser.parse_args()
+
+if not args.retriever_eval_only:
+    raise Exception('Must specifify -RO argument (of 1 or 0) indicating running pipeline for retriever-only or for full system.')
+else:
+    retriever_eval_only = bool(args.retriever_eval_only)
+
+print(retriever_eval_only)
 
 # run data prep pipeline
 if not os.path.exists(os.path.join(module_path,'data/stage_data')):
-    data_utils.data_prep_pipeline()
+    data_utils.data_prep_pipeline(retriever_eval_only)
 
 # run data compilation pipeline
 if not os.path.exists(os.path.join(module_path,'data/eval_data')):
-    data_utils.compile_data_pipeline()
+    data_utils.compile_data_pipeline(retriever_eval_only)
